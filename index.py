278e23b78d30bae93f09a020b59253c0135d41b9
Caio França <caiobdafranca@gmail.com>
2023-03-26 23:05:31 -0300

    ✨feat: Execução de modelo y1

--- index.py
+++ index.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from supabase import create_client, Client
-import pickle
+import os
+import requests
 
 app = Flask(__name__)
 
-SUPABASE_URL = "https://btqrhxskatfcyasskvtz.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ0cXJoeHNrYXRmY3lhc3NrdnR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQ2NjMxODQsImV4cCI6MTk4MDIzOTE4NH0.c2A5FcLIQEvFfAkMg1Fmw_iZQ2SIB6BOxahqnHEA_oM"
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
 
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+y1_model_execute = os.environ.get("GOOGLE_CLOUD_MODEL_Y1_URL")
+
+supabase: Client = create_client(url, key)
 
 
 @app.route('/')
@@ -15,20 +18,17 @@ def home():
     return 'Home'
 
 
-@app.route('/execute_y1_model')
+@app.route('/execute_model_y1')
 def about():
-    with open('model_y1.pkl', 'rb') as f:
-        model = pickle.load(f)
-
     response = (supabase.table('data').select("T_inj_control, GG_rpm, HP_T_exit_temp, timestamp").order(
         column="timestamp", desc=True).limit(1).single().execute()).data
 
-    test = [[response["T_inj_control"],
-             response["GG_rpm"], response["HP_T_exit_temp"]]]
+    payload = {"values": [[response["T_inj_control"],
+                           response["GG_rpm"], response["HP_T_exit_temp"]]]}
 
-    result = model.predict(test)
+    result = requests.post(y1_model_execute, json=payload)
 
     supabase.table('predict_outputs').insert(
-        {"value": result[0], "timestamp": response["timestamp"], "model_output_name": "y1"}).execute()
+        {"value": result.text, "timestamp": response["timestamp"], "model_output_name": "y1"}).execute()
 
-    return True
+    return result.text
